09b8b8527b81d7acf262e9068d76ff4d7444102a
sodoemploy <105046731+sodoemploy@users.noreply.github.com>
2023-05-06 22:02:31 +0800

    Update index.js

--- cmt real time/index.js
+++ cmt real time/index.js
@@ -2,25 +2,25 @@ import WebSocket, { WebSocketServer } from "ws";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-const wss = new WebSocketServer({ port: 3000 });
+const wss = new WebSocketServer({ port: 80 });
 
-
-wss.on("connection", function connection(ws,req) {
-
+wss.on("connection", function connection(ws, req) {
   ws.on("message", function message(message) {
     const data = JSON.parse(message);
- 
 
     if (data.type === "message") {
       wss.clients.forEach((client) => {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
           client.send(
-            JSON.stringify({ type: "message", data: data.data, room: data.room, username: data.username })
+            JSON.stringify({
+              type: "message",
+              data: data.data,
+              room: data.room,
+              username: data.username,
+            })
           );
         }
       });
     }
   });
 });
-
-
